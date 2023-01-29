f2eda25082388385352dd7dbf4090e2ef2febacd
Ashlee Gudeman <ashleegudeman@Ashlees-MBP.attlocal.net>
2023-01-29 17:44:34 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+    let firstName = "Ashlee";
+    let lastName = "Gudeman";
+    let age = 34;
+
+alert("My name is " + firstName + " " + lastName + ".");
+alert("I am " + age + " years old.");
+
+console.log("This class is 10/10.");
